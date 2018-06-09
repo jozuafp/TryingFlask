d38d998e710e2c38307b6d8154ed5b02fca57843
jozua palandi <jozuafp@gmail.com>
2018-06-09 16:24:44 +0700

    first commit

--- pengujian.py
+++ pengujian.py
@@ -1,25 +1,3 @@
-# Import required libraries
-import os
-from random import randint
-
-import plotly.plotly as py
-from plotly.graph_objs import *
-
-import flask
-import dash
-from dash.dependencies import Input, Output, State, Event
-import dash_core_components as dcc
-import dash_html_components as html
-
-
-# Setup the app
-# Make sure not to change this file name or the variable names below,
-# the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
-
-
 # Put your Dash code here
 import numpy as np
 import pandas as pd
@@ -71,8 +49,3 @@ grid.fig.tight_layout(w_pad=1)
 plt.show()
 
 sns.set(style="ticks")
-
-# Run the Dash app
-if __name__ == '__main__':
-    app.server.run(debug=True, threaded=True)
-
